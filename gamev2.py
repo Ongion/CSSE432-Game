b67892216042184e6a62fc1ba3ba20a7aa73133c
Benjamin Paul <benjamin.ian.paul@gmail.com>
2015-05-13 19:48:57 -0400

    Added more in GameState, wrapped screen for ships.

--- gamev2.py
+++ gamev2.py
@@ -1,13 +1,13 @@
 import math
 
-WIDTH = 640
-HEIGHT = 480
+WIDTH = 900
+HEIGHT = 900
 FRAME_PERIOD = 1000/60
 A_SHIP = 10
 V_SHIPTURN = 5
 V_LASER = 10
 LASER_LENGTH = 10
-
+SHIP_LENGTH = 20
 START_CIRCLE_RADIUS = 30
 
 class GameState:
@@ -17,6 +17,7 @@ class GameState:
 		self.width = WIDTH
 		self.height = HEIGHT
 		self.ships = []
+		self.lives = [3 for x in range(numPlayers)]
 		centerX = self.width/2
 		centerY = self.height/2
 		for i in range(numPlayers):
@@ -25,6 +26,15 @@ class GameState:
 			offsetX = START_CIRCLE_RADIUS * math.sin(2 * math.pi * i / numPlayers)
 			self.ships[i] = Spaceship(self, centerX + offsetX, centerY + offsetY, 2 * math.pi * i / numPlayers, i)
 		self.lasers = []
+
+	def update(self):
+		for ship in self.ships:
+			ship.update()
+		for laser in self.lasers:
+			laser.update()
+		# APPLY COLLISION DETECTION HERE
+		# Now, remove the lasers that are offscreen!
+		self.lasers = [laser for laser in lasers if not laser.isOffScreen()]
 
 
 class Spaceship:
@@ -52,6 +62,17 @@ class Spaceship:
 	def update(self):
 		self.x += self.vX * FRAME_PERIOD
 		self.y += self.vY * FRAME_PERIOD
+		self.doScreenWraps()
+
+	def doScreenWraps(self):
+		if self.x < -SHIP_LENGTH:
+			self.x += self.game.width + 2 * SHIP_LENGTH
+		elif self.x > self.game.width + SHIP_LENGTH:
+			self.x -= self.game.width + 2* SHIP_LENGTH 
+		if self.y < -SHIP_LENGTH:
+			self.y += self.game.height + 2 * SHIP_LENGTH
+		elif self.y > self.game.height + SHIP_LENGTH
+			self.y -= self.game.height + 2 * SHIP_LENGTH
 
 class Laser:
 	
