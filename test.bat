a1146b1e11785b53e2c8aa82aaa09327140342c2
unknown <petryjc@PETRYJC-W520.rose-hulman.edu>
2015-05-18 03:22:19 -0400

    updating test thing

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
 @echo off
-start cmd /k "python gamev3.py"
-start cmd /k "python gamev3.py -p 8001"
+start cmd /k "python game.py"
+start cmd /k "python game.py -p 8001"
 ::start cmd /k "python networking.py -p 8002"
 ::start cmd /k "python networking.py -p 8003"
