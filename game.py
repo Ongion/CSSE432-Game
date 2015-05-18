6897b458b26c8bbcd38b4fe029ccc6cd4c3dfd88
unknown <petryjc@PETRYJC-W520.rose-hulman.edu>
2015-05-18 03:21:48 -0400

    Stuff works

--- game.py
+++ game.py
@@ -1,181 +1,304 @@
-'''
-Created on May 9, 2015
+import math, pygame,sys,pygame.locals
+import networking
+import threading
+import multiprocessing
+import time
 
-@author: schackma
-'''
-import math
-from tkinter import Tk, Canvas, Frame, BOTH
+WIDTH = 900
+HEIGHT = 900
+FRAME_PERIOD = 1000/60
+A_SHIP = 3
+V_SHIPTURN = 10
+V_LASER = 10
+LASER_LENGTH = 10
+SHIP_LENGTH = 20
+START_CIRCLE_RADIUS = 30
+SHIP_ANGLE = math.pi/3
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+GLOBAL_FRAME_RATE = 15
 
-WIDTH = 1000
-HEIGHT = 1000
-DELAY = 17
-SPEED = 10
-ASPEED = math.pi/10
-LSPEED = 10
 
-class Board(Canvas):
+class Keyboard_Monitor(threading.Thread):
+  def __init__(self, gameState):
+    threading.Thread.__init__(self)
+    self.gameState = gameState
+
+  def run(self):
+    while True:
+      self.gameState.print_menu()
+      i = input("")
+      print("Got command ", i)
+      if (i == "1"):
+        self.gameState.gameManager.send_connection_request()
+      elif (i == "2"):
+        self.gameState.my_time = time.time()
+        self.gameState.playRequests.append(self.gameState.my_time)
+        time.sleep(0.01)
+        self.gameState.gameManager.broadcast({"type": "play","time": self.gameState.my_time})
+      else:
+        print("Unknown Request")
+    
+
+class GameState:
+
+  def __init__(self):
+    self.width = WIDTH
+    self.height = HEIGHT
+    self.ships = []
+    self.spriteShips = pygame.sprite.Group()
+    self.spriteLasers = pygame.sprite.Group()
+    self.playRequests = []
+    #we won't know our number until a play transaction has transpired
+    self.id = None
+    
+    #set up the game manager that does all of the threads for sending and receiving messages
+    self.gameManager = networking.GameManager()
+    self.gameManager.parseArgs(sys.argv[1:])
+    t = threading.Thread(target=self.gameManager.main,args=(self,))
+    t.start()
+    
+    #set up the keyboard monitor that listens for requests
+    self.keyboardM = Keyboard_Monitor(self)
+    self.keyboardM.start()
+    
+    self.update_count = 0
+    print("Here")
+    
+  def print_menu(self):
+    print("You are presently connected to " + str(self.gameManager.connection_ports) + " peers")
+    print("Press...")
+    print("1....Send connection request")
+    print("2....Play")
+    
+  def run(self):
+    centerX = self.width/2
+    centerY = self.height/2
+    numPlayers = len(self.playRequests)
+    for i in range(numPlayers):
+      # Start all the players out in a circle around the center, facing outwards
+      offsetY = START_CIRCLE_RADIUS * math.cos(2 * math.pi * i / numPlayers)
+      offsetX = START_CIRCLE_RADIUS * math.sin(2 * math.pi * i / numPlayers)
+      self.ships.append(Spaceship(self, self.width/2 + offsetX, centerY + offsetY, 2 * math.pi * i / numPlayers, i))
+      self.spriteShips.add(self.ships[i])
+    self.lasers = []
   
-    def __init__(self, parent):
-        Canvas.__init__(self, width=WIDTH, height=HEIGHT, 
-            background="black", highlightthickness=0)
-         
-        self.parent = parent 
-        self.ship = Spaceship(50,50)
-        self.lasers = []
-        self.ships = [self.ship,Spaceship(100,50)]
-        
-        self.bind_all("<Key>",self.onKeyPressed)
-        self.after(DELAY, self.onTimer)
-        self.pack()        
-    
-    def updateUI(self):
-      self.updateShipUI()
-      updateLaserUI(self)
-    
-    def updateShipUI(self):
-        #delete previous ships from the UI
-        oldShips = self.find_withtag("ship");
-        for s in oldShips:
-            self.delete(s)
-            
-        for s in self.ships:
-            self.create_polygon(s.points(),outline = 'white',tag='ship')
-    
-    def updateLaserUI(self):
-        for l in self.lasers:
-            self.move(self.lasers[x].drawing,LSPEED*math.cos(self.lasers[x].angle),-LSPEED*math.sin(self.lasers[x].angle))
-            
-            x = x+1
-    
-    
-    def onKeyPressed(self, e): 
-    
-        key = e.keysym
+    pygame.init()
+    pygame.key.set_repeat(60,60)
+    screen = pygame.display.set_mode((WIDTH,HEIGHT))
+    screen.fill(BLACK)
+    clock= pygame.time.Clock()
 
-        if key == "Left": 
-            self.ship.angle += ASPEED
-        
-
-        if key == "Right":
-            self.ship.angle += -ASPEED
-        
-
-        if key == "Up":
-            self.ship.vx += SPEED*math.cos(self.ship.angle)
-            self.ship.vy += -SPEED*math.sin(self.ship.angle)
-        
-
-        if key == "Down":
-            self.ship.vx += -SPEED*math.cos(self.ship.angle)
-            self.ship.vy += SPEED*math.sin(self.ship.angle)
-        
-        if key == "a":
-            self.ship.fire = True
-    
-           
-    def createLaser(self,ship):
-        Lwidth = 25
-        Lhieght = 10
-        x = self.create_line(ship.x,ship.y,
-                              ship.x-Lhieght*math.cos(ship.angle)+Lwidth*math.cos(ship.angle),ship.y-Lwidth*math.sin(ship.angle)-Lhieght*math.sin(ship.angle),
-                              fill = 'white',tag='laser')
-        las = Laser(ship.angle,x)
-        self.lasers.append(las)
-        
-        
-    def checkCollisions(self):
-        for l in self.find_withtag("ship"):
-            Lc = self.coords(l)
-            overlap = self.find_overlapping(Lc[0],Lc[1],Lc[2],Lc[3])
-            for over in overlap:
-                print(self.gettags(over))
-                if self.gettags(over) ==('laser'):
-                    print("true")
-                    c = self.coords(over)
-                    for ship in self.ships:
-                        if c[0] == ship.x:
-                            self.ships.remove(ship)
-                            break
-                        self.delete(over)
-                
-                  
-        
-        
-    def onTimer(self):
-        self.ship.move()
-        self.updateUI()
-        self.moveLasers()
-        self.checkCollisions()
-        
-        if self.ship.fire:
-            self.ship.fire = False
-            self.createLaser(self.ship)
-            
-        self.after(DELAY, self.onTimer)
-        
-class Spaceship:
-    length = 50
-    innerAngle = math.pi/6
-    
-    def __init__(self,startX,startY):
-        self.x = startX
-        self.y = startY
-        self.vx = 0
-        self.vy = 0
-        self.angle = 0
-        self.fire = False
-        self.timestep = DELAY/1000
-    
-    def addVel(self,x):
-        self.vx = self.vx + x*math.cos(self.angle)
-        self.vy = self.vy +x*math.sin(self.angle)
-    
-    def move(self):
-        self.x = self.x+self.vx*self.timestep
-        self.y = self.y+self.vy*self.timestep
-        if self.x <0:
-            self.x = WIDTH
-        
-        if self.x > WIDTH:
-            self.x = 0
-        
-        if self.y < 0:
-            self.y = HEIGHT
-        
-        if self.y > HEIGHT:
-            self.y = 0
-            
-    def points(self):
-      return [self.x,self.y,
-                  self.x-self.length*math.cos(self.angle+self.innerAngle),self.y+self.length*math.sin(self.angle+self.innerAngle),
-                  self.x-self.length*math.cos(self.angle-self.innerAngle),self.y+self.length*math.sin(self.angle-self.innerAngle)]
-        
-        
-    def turn(self,x):
-        self.angle = self.angle+x
-
-class Laser:
-    
-    def __init__(self,angle,draw):
-        self.angle = angle
-        self.drawing = draw
+    self.spriteShips.draw(screen)
+    pygame.display.flip()
 
 
-class game(Frame):
+    while True:
+      for event in pygame.event.get():#30
+        if event.type==pygame.locals.QUIT:#31
+          pygame.quit()#32
+          sys.exit()#33
+        if event.type == pygame.KEYDOWN:
+          self.calculateMove(event)
+      self.update()
+      screen.fill(BLACK)
+      self.spriteShips.draw(screen)
+      self.spriteLasers.draw(screen)
+      pygame.display.flip()
+      clock.tick(60)
+    
+  def dispatch(self, m):
+    if "type" in m:
+      if m["type"] == "play":
+        self.playCommand(m)
+      elif m["type"] == "shipLocation":
+        self.ships[m["id"]].x = m["x"]
+        self.ships[m["id"]].y = m["y"]
+        self.ships[m["id"]].vX = m["vX"]
+        self.ships[m["id"]].vY = m["vY"]
+        self.ships[m["id"]].angle = m["angle"]
+      elif m["type"] == "laserCreation":
+        laser = Laser(self,m["x"],m["y"],m["angle"],m["playerNum"])
+        self.spriteLasers.add(laser)
+        self.lasers.append(laser)
+      else:
+        print("Message Type Unsupported " + str(m["type"]))
+    else:
+      print("Invalid message: no given type "+ str(m))
+    
+  def playCommand(self, m):
+    if self.playRequests:
+      self.playRequests.append(m["time"])
+    else:
+      self.my_time = time.time()
+      self.playRequests.append(m["time"])
+      self.playRequests.append(self.my_time)
+      self.gameManager.broadcast({"type": "play","time": self.my_time})
+      
+    print(self.playRequests)
+    
+    #we should have a play request from everyone and one from ourselves
+    if len(self.playRequests) == len(self.gameManager.connection_ports) + 1:
+      #theoretically, they would be ordered, but we have no guarantees...unless we sort it
+      self.playRequests.sort()
+      self.id = self.playRequests.index(self.my_time)
+      #we are no longer listening for commands from our keyboard for setting up connections/sending requests
+      time_to_wait = 3 - (time.time() - self.playRequests[-1])
+      print(time_to_wait)
+      time.sleep(time_to_wait)
+      self.run()
+      
+  def update(self):
+    for ship in self.ships:
+      ship.update()
+    for laser in self.lasers:
+      laser.update()
+    self.collision()
+    # Now, remove the lasers that are offscreen!
+    self.lasers = [laser for laser in self.lasers if not laser.isOffScreen()]
+    
+    #only send updates to your friends every GLOBAL_FRAME_RATE times
+    self.update_count += 1
+    if self.update_count >= GLOBAL_FRAME_RATE:
+      self.update_count = 0
+      #print("broadcasting location")
+      self.gameManager.broadcast({"type": "shipLocation","id":self.id,"x":self.ships[self.id].x,"y":self.ships[self.id].y,"angle":self.ships[self.id].angle,"vX":self.ships[self.id].vX,"vY":self.ships[self.id].vY})
+      time.sleep(0.1)
+    
+  def calculateMove(self,event):
+    rotate = accel = 0
+    if event.key == 276: #LEFT
+      rotate = -.01
+    elif event.key == 275: #RIGHT
+      rotate = .01
+    elif event.key == 273: #UP
+      accel = .01
+    elif event.key == 274:#DOWN
+      accel = -.01
+    elif event.key == 32:
+      laser = Laser(self,game.ships[self.id].rect.centerx,game.ships[self.id].rect.centery,game.ships[self.id].angle,game.ships[self.id].playerNum)
+      self.spriteLasers.add(laser)
+      self.lasers.append(laser)
 
-    def __init__(self, parent):
-        Frame.__init__(self, parent)
-                
-        parent.title('Space Wars')
-        self.board = Board(parent)
-        self.pack()  
-        
-def main():
+    self.ships[self.id].applyInput([rotate,accel])  
+    
+  def collision(self):
+    for laser in self.spriteLasers:
+
+      # See if it hit a block
+      hit_list = pygame.sprite.spritecollide(laser, self.spriteShips, False)
+
+      # For each block hit, remove the bullet and add to the score
+      for ship in hit_list:
+        if(ship.playerNum != laser.playerNum):
+          print(ship)
+          self.spriteShips.remove(ship)
+          
+
+      # Remove the bullet if it flies up off the screen
+      if laser.isOffScreen():
+        self.spriteLasers.remove(laser)
+
+    
+    
+class Spaceship(pygame.sprite.Sprite):
+
+  def __init__(self, game, startX, startY, startAngle, playerNum):
+    self.game = game
+    self.x = startX
+    self.y = startY
+    self.playerNum = playerNum
+    self.angle = startAngle
+    self.vX = 0
+    self.vY = 0
+    
+    super().__init__()
+    self.original = pygame.Surface((SHIP_LENGTH,SHIP_LENGTH))
+    self.rect = self.original.get_rect()
+    self.original.set_colorkey(BLACK)
+    self.rect.x = self.x
+    self.rect.y = self.y
+    pygame.draw.polygon(self.original,WHITE,self.points(),0)
+    self.image = pygame.transform.rotate(self.original,-self.angle*180/math.pi)
+
+    
+  def points(self):
+    return ((SHIP_LENGTH/2+math.sqrt(3)/3*SHIP_LENGTH,SHIP_LENGTH/2),
+      (SHIP_LENGTH/2+math.sqrt(3)/3*SHIP_LENGTH*math.cos(math.pi-SHIP_ANGLE),SHIP_LENGTH/2+math.sqrt(3)/3*SHIP_LENGTH*math.sin(math.pi-SHIP_ANGLE)),
+      (SHIP_LENGTH/2+math.sqrt(3)/3*SHIP_LENGTH*math.cos(math.pi+SHIP_ANGLE),SHIP_LENGTH/2+math.sqrt(3)/3*SHIP_LENGTH*math.sin(math.pi+SHIP_ANGLE)))  
+      
+      
+  def applyInput(self, input):
+    self.rotationInput(input[0])
+    self.velocityInput(input[1])
+
+  def rotationInput(self, inputX):
+    self.angle += inputX * V_SHIPTURN
+
+  def velocityInput(self, inputY):
+    self.vX += A_SHIP * inputY * math.cos(self.angle)
+    self.vY += A_SHIP * inputY * math.sin(self.angle)
+
+  def update(self):
+    self.x += self.vX * FRAME_PERIOD
+    self.y += self.vY * FRAME_PERIOD
+    self.doScreenWraps()
+    self.rect.x = self.x
+    self.rect.y = self.y
+    self.image = pygame.transform.rotate(self.original,-self.angle*180/math.pi)
+
+  def doScreenWraps(self):
+    if self.x < -SHIP_LENGTH:
+      self.x += self.game.width + 2 * SHIP_LENGTH
+    elif self.x > self.game.width + SHIP_LENGTH:
+      self.x -= self.game.width + 2* SHIP_LENGTH 
+    if self.y < -SHIP_LENGTH:
+      self.y += self.game.height + 2 * SHIP_LENGTH
+    elif self.y > self.game.height + SHIP_LENGTH:
+      self.y -= self.game.height + 2 * SHIP_LENGTH
+
+class Laser(pygame.sprite.Sprite):
   
-    root = Tk()
-    ex = game(root)
-    root.mainloop()  
+  def __init__(self, game, startX, startY, angle, playerNum):
+    self.game = game
+    self.x = startX
+    self.y = startY
+    self.angle = angle
+    self.playerNum = playerNum
+    
+    super().__init__()
+    self.image = pygame.Surface((LASER_LENGTH,LASER_LENGTH))
+    self.rect = self.image.get_rect()
+    self.image.set_colorkey(BLACK)
+    self.rect.x = self.x
+    self.rect.y = self.y
+    pygame.draw.line(self.image,WHITE,self.points1(),self.points2(),1)
+    
+    #if we are the on shooting, we need to tell everybody
+    if playerNum == self.game.id:
+      self.game.gameManager.broadcast({"type": "laserCreation", "x": self.x, "y": self.y, "angle": self.angle, "playerNum": self.playerNum})
+  
+  def points1(self):
+    return (LASER_LENGTH/2 - LASER_LENGTH/2*math.cos(self.angle),LASER_LENGTH/2-LASER_LENGTH/2*math.sin(self.angle))
+  
+  def points2(self):
+    return (LASER_LENGTH/2 + LASER_LENGTH/2*math.cos(self.angle),LASER_LENGTH/2+LASER_LENGTH/2*math.sin(self.angle))
+
+  def update(self):
+    self.x += V_LASER * math.cos(self.angle)
+    self.y += V_LASER * math.sin(self.angle)
+    self.rect.x = self.x
+    self.rect.y = self.y
+
+  def isOffScreen(self):
+    xLen = LASER_LENGTH * math.cos(self.angle)
+    yLen = LASER_LENGTH * math.sin(self.angle)
+    return self.x < -xLen or self.x > game.width + xLen or self.y < -yLen or self.y > game.height + yLen
 
 
-if __name__ == '__main__':
-    main()  
+#Main game
+if __name__ == "__main__":
+  print(sys.getcheckinterval())
+  sys.setcheckinterval(1)
+  game = GameState()
+
--- networking.py
+++ networking.py
@@ -6,108 +6,14 @@ import select
 import json
 import time
 
-localPort = 8000
-localHost = "localhost" # change to "" to run over outside network
-connected_peers = []
-connection_ports = []
-
-#sends broadcast to all connected peers.  Assumes input can be converted into json (like a dict)
-def broadcast(message):
-  m = bytes(json.dumps(message), 'UTF-8')
-  for peer in connected_peers:
-    peer.send(m)
-    
-def receive_message(handler):
-  while True:
-    if (not any(connected_peers)):
-      time.sleep(1)
-      continue
-    try: 
-      inputready, outputready, exceptready = select.select(connected_peers, [], [], 1)
-    except select.error as err:
-      print("SELECT error: {0}".format(err))
-      break
-    except socket.error as err:
-      print("SELECT error: {0}".format(err))
-      break
-    
-    for s in inputready:
-      buf = s.recv(1024)
-      m = json.loads(buf.decode("UTF-8"))
-      handler.dispatch(m)
-  print("Receiver Stopped")
-  
-def parseArgs(argv):
-  try:
-    opts, args = getopt.getopt(argv,"p:",["port="])
-  except getopt.GetoptError:
-    print ('test.py -p <port>')
-    sys.exit(2)
-  for opt, arg in opts:
-    if opt == '-h':
-       print ('test.py -p <port>')
-       sys.exit()
-    elif opt in ("-p", "--port"):
-      try:
-        global localPort
-        localPort = int(arg)
-      except ValueError:
-        print ("Port must be an integer")
-        sys.exit()
-  print (localPort)
-
-def send_connection_request():
-  opponent_port = input("opponent port: ")
-  opponent_ip = input("opponent ip: ")
-  try: 
-    opponent_port = int(opponent_port)
-  except:
-    print("Opponent port must be an integer")
-    return send_connection_request()
-  
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  s.connect((opponent_ip, opponent_port))
-  s.send(bytes(json.dumps({"type": "connection_request", "peers" : connection_ports, "me" : (localHost,localPort)}), 'UTF-8'))
-  buf = s.recv(1024)
-  response = json.loads(buf.decode("UTF-8"))
-  print("R1" + str(response))
-  if response["type"] == "connection_response":
-    for other_peer in response["peers"]:
-      s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      s1.connect((other_peer[0],other_peer[1]))
-      s1.send(bytes(json.dumps({"type": "connection_finish", "peers" : connection_ports, "me" : (localHost,localPort)}), 'UTF-8'))
-      buf = s1.recv(1024)
-      connected_peers.append(s1)
-      #ignore the response of the others (we don't really care what they have to say)
-    connected_peers.append(s)
-    connection_ports.append((opponent_ip,opponent_port))
-    connection_ports.extend(response["peers"])
-  else:
-    print("Send Connection Error")
-    print(response)
-      
-class Keyboard_Monitor(threading.Thread):
-  def run(self):
-    while True:
-      print_menu()
-      i = input("")
-      print("Got command ", i)
-      if (i == "1"):
-        send_connection_request()
-      elif (i == "2"):
-        broadcast({"type": "play"})
-      elif i == "3":
-        broadcast({"type": "a"})
-      else:
-        print("Unknown Request")
-    
 class Receive_Connection(threading.Thread):
-  def __init__(self):
+  def __init__(self, gameManager):
     threading.Thread.__init__(self)
     self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.gameManager = gameManager
   
   def run(self):
-    self.socket.bind((localHost, localPort))
+    self.socket.bind((self.gameManager.localHost, self.gameManager.localPort))
     self.socket.listen(10)
     
     while True:
@@ -120,60 +26,140 @@ class Receive_Connection(threading.Thread):
         for other_peer in response["peers"]: # connect to everyone he is already connected to
           s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
           s1.connect((other_peer[0],other_peer[1]))
-          s1.send(bytes(json.dumps({"type": "connection_finish", "peers" : [], "me" : (localHost,localPort)}), 'UTF-8'))
+          s1.send(bytes(json.dumps({"type": "connection_finish", "peers" : [], "me" : (self.gameManager.localHost,self.gameManager.localPort)}), 'UTF-8'))
           buf = s1.recv(1024)
-          connected_peers.append(s1)
-        connection.sendall(bytes(json.dumps({"type": "connection_response", "peers" : connection_ports}), 'UTF-8'))
-        connected_peers.append(connection)
-        connection_ports.extend(response["peers"])
-        connection_ports.append(response["me"])
+          self.gameManager.connected_peers.append(s1)
+        connection.sendall(bytes(json.dumps({"type": "connection_response", "peers" : self.gameManager.connection_ports}), 'UTF-8'))
+        self.gameManager.connected_peers.append(connection)
+        self.gameManager.connection_ports.extend(response["peers"])
+        self.gameManager.connection_ports.append(response["me"])
       elif response["type"] == "connection_finish":
         print ("Received connection finish")
         for other_peer in response["peers"]: # connect to everyone he is already connected to
           s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
           s1.connect((other_peer[0],other_peer[1]))
-          s1.send(bytes(json.dumps({"type": "connection_request", "peers" : [], "me" : (localHost,localPort)}), 'UTF-8'))
+          s1.send(bytes(json.dumps({"type": "connection_request", "peers" : [], "me" : (self.gameManager.localHost,self.gameManager.localPort)}), 'UTF-8'))
           buf = s1.recv(1024)
-          connected_peers.append(s1)
+          self.gameManager.connected_peers.append(s1)
         connection.sendall(bytes(json.dumps({"type": "connection_response", "peers" : connection_ports}), 'UTF-8'))
-        connected_peers.append(connection)
-        connection_ports.extend(response["peers"])
-        connection_ports.append(response["me"])
+        self.gameManager.connected_peers.append(connection)
+        self.gameManager.connection_ports.extend(response["peers"])
+        self.gameManager.connection_ports.append(response["me"])
       else:
         print("Receive Connection Error.  Received invalid request to port listening for requests")
         print(response)
       
-def print_menu():
-  global connected_peers
-  print("You are presently connected to " + str(connection_ports) + " peers")
-  print("Press...")
-  print("1....Send connection request")
-  print("2....Play")
-    
-class TestClass():
-  def dispatch(self, m):
-    if m["type"] == "a":
-      self.a(m)
+class GameManager():
+  def __init__(self):
+    self.localPort = 8000
+    self.localHost = "localhost" # change to "" to run over outside network
+    self.connected_peers = []
+    self.connection_ports = []
 
-  def a(self, m):
-    print ("This ran a")
+  def main(self, c):
     
-def main(c):
-  t1 = Keyboard_Monitor()
-  t1.start()
-  
-  t2 = Receive_Connection()
-  t2.start()
-  
-  t3 = threading.Thread(target=receive_message,args=(c,))
-  t3.start()
-  
-  t1.join()
-  t2.join()
-  t3.join()
+    t2 = Receive_Connection(self)
+    t2.start()
     
-if __name__ == "__main__":
-  parseArgs(sys.argv[1:])
-  
-  main(TestClass())
-  
+    t3 = threading.Thread(target=self.receive_message,args=(c,))
+    t3.start()
+    
+    t2.join()
+    t3.join()
+    
+    
+  #sends broadcast to all connected peers.  Assumes input can be converted into json (like a dict)
+  def broadcast(self, message):
+    m = bytes(json.dumps(message), 'UTF-8')
+    
+    for peer in self.connected_peers:
+      peer.send(m)
+      
+  def receive_message(self, handler):
+    while True:
+      if (not any(self.connected_peers)):
+        time.sleep(1)
+        continue
+      try: 
+        inputready, outputready, exceptready = select.select(self.connected_peers, [], [], 1)
+      except select.error as err:
+        print("SELECT error: {0}".format(err))
+        break
+      except socket.error as err:
+        print("SELECT error: {0}".format(err))
+        break
+      for s in inputready:
+        try:
+          buf = s.recv(1024)
+          s = buf.decode("UTF-8")
+          #this hack exists because we could get multiple json objects at the same time and need to parse them all separately.  We really should use some kind of message bundler and if this needs to happen use a regex to parse it faster (though at least this still operates in linear time)
+          l = []
+          c = 0
+          s1 = ""
+          for i in s:
+            s1 += i
+            if i == "{" or i == "(":
+              c += 1
+            elif i == "}" or i == ")":
+              c -= 1
+              if c == 0:
+                l.append(s1)
+                s1 = ""
+                
+          for s in l:
+            m = json.loads(s)
+            threading.Thread(target=handler.dispatch,args=(m,)).start()
+        except:
+          print("Unexpected error")
+        
+    print("Receiver Stopped")
+    
+  def parseArgs(self, argv):
+    try:
+      opts, args = getopt.getopt(argv,"p:",["port="])
+    except getopt.GetoptError:
+      print ('test.py -p <port>')
+      sys.exit(2)
+    for opt, arg in opts:
+      if opt == '-h':
+         print ('test.py -p <port>')
+         sys.exit()
+      elif opt in ("-p", "--port"):
+        try:
+          self.localPort = int(arg)
+        except ValueError:
+          print ("Port must be an integer")
+          sys.exit()
+
+  def send_connection_request(self):
+    opponent_port = input("opponent port: ")
+    opponent_ip = input("opponent ip: ")
+    try: 
+      opponent_port = int(opponent_port)
+    except:
+      print("Opponent port must be an integer")
+      return self.send_connection_request()
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((opponent_ip, opponent_port))
+    s.send(bytes(json.dumps({"type": "connection_request", "peers" : self.connection_ports, "me" : (self.localHost,self.localPort)}), 'UTF-8'))
+    buf = s.recv(1024)
+    response = json.loads(buf.decode("UTF-8"))
+    print("R1" + str(response))
+    if response["type"] == "connection_response":
+      for other_peer in response["peers"]:
+        s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s1.connect((other_peer[0],other_peer[1]))
+        s1.send(bytes(json.dumps({"type": "connection_finish", "peers" : connection_ports, "me" : (localHost,localPort)}), 'UTF-8'))
+        buf = s1.recv(1024)
+        self.connected_peers.append(s1)
+        #ignore the response of the others (we don't really care what they have to say)
+      self.connected_peers.append(s)
+      self.connection_ports.append((opponent_ip,opponent_port))
+      self.connection_ports.extend(response["peers"])
+    else:
+      print("Send Connection Error")
+      print(response)
+     
+     
+    
